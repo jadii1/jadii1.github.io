0dc0ad67eeaf99b93b012241eeb98ee26df3e334
jadii1 <jawadahmed7788666@gmail.com>
2026-02-08 23:35:13 +0500

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,3 +1,17 @@
+/* --- META PIXEL BASE CODE START --- */
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '1428695112133048'); 
+fbq('track', 'PageView');
+/* --- META PIXEL BASE CODE END --- */
+
 const menuToggle = document.getElementById("menu-toggle");
 const navLinks = document.getElementById("nav-links");
 
@@ -11,6 +25,7 @@ const greeting = document.getElementById("userGreeting");
 const loginBtn = document.getElementById("login-btn");
 const registerBtn = document.getElementById("register-btn");
 const logoutBtn = document.getElementById("logout-btn");
+
 window.addEventListener("DOMContentLoaded", () => {
   const user =
     JSON.parse(localStorage.getItem("user")) ||
@@ -20,33 +35,36 @@ window.addEventListener("DOMContentLoaded", () => {
     const email = user.email;
     const username = email.split("@")[0];
     greeting.textContent = `Hello, ${username}`;
-    greeting.style.display = "inline"; // or "block"
+    greeting.style.display = "inline"; 
     greeting.style.textTransform = "capitalize";
     loginBtn.style.display = "none";
     registerBtn.style.display = "none";
-    logoutBtn.style.display = "inline"; // or "block"
+    logoutBtn.style.display = "inline"; 
   } else {
-    userMenu.style.display = "none";
+    // Check if userMenu exists before styling to avoid errors
+    if(userMenu) userMenu.style.display = "none";
   }
 });
 
 function logout() {
   localStorage.removeItem("user");
   sessionStorage.removeItem("user");
-  window.location.reload(); // or redirect to login
+  window.location.reload(); 
 }
 
 // user-menu toggle
 const userMenuButton = document.getElementById("userMenuButton");
 const userDropdown = document.getElementById("userDropdown");
 
-userMenuButton.addEventListener("click", () => {
-  userDropdown.classList.toggle("hidden");
-});
+if (userMenuButton && userDropdown) {
+  userMenuButton.addEventListener("click", () => {
+    userDropdown.classList.toggle("hidden");
+  });
 
-// Close dropdown when clicking outside
-document.addEventListener("click", (e) => {
-  if (!userMenuButton.contains(e.target) && !userDropdown.contains(e.target)) {
-    userDropdown.classList.add("hidden");
-  }
-});
+  // Close dropdown when clicking outside
+  document.addEventListener("click", (e) => {
+    if (!userMenuButton.contains(e.target) && !userDropdown.contains(e.target)) {
+      userDropdown.classList.add("hidden");
+    }
+  });
+}
